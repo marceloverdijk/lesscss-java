3d821342c5c53919db537b41752a737d9395ebb7
Charlie Hubbard <charlie@greatparentsacademy.com>
2014-01-05 15:37:45 -0500

    Added a Compile class to run from the command line.

---
+++ src/main/java/org/lesscss/Compile.java
@@ -0,0 +1,27 @@
+package org.lesscss;
+
+import org.lesscss.logging.LessLogger;
+import org.lesscss.logging.LessLoggerFactory;
+
+import java.io.File;
+
+public class Compile {
+
+    private static final LessLogger logger = LessLoggerFactory.getLogger( Compile.class );
+
+	public static void main(String[] args) throws Exception {
+		if( args.length < 1 ) {
+		    logger.info("usage: org.lesscss.Compile <less_filename>");
+            System.exit(-1);
+		}
+
+        File output = new File( args[0] + ".css" );
+        logger.info("Compiler output = %s", output.getCanonicalPath() );
+
+        long start = System.currentTimeMillis();
+		LessCompiler lessCompiler = new LessCompiler();
+		lessCompiler.compile( new File( args[0] ), output );
+        long duration = System.currentTimeMillis() - start;
+        logger.info("Done. %,d ms", duration);
+	}
+}
